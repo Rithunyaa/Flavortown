ea019a459c8fc46bf5170cfe3b76e5245d563c0c
Rithunyaa <rithunyaa@gmail.com>
2026-01-23 13:46:53 -0500

    Add biology-related flashcards to script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const flashcards = [
-  { question: "What is HTML?", answer: "A markup language for web pages" },
-  { question: "What is CSS?", answer: "Styles the appearance of web pages" },
-  { question: "What is JavaScript?", answer: "Adds interactivity to websites" }
+  { question: "Lysosomes", answer: "An organelle that contains destructive enzymes and is used for cell waste cleanup" },
+  { question: "Centrioles", answer: "Barrel-shaped organelles that organize the cell’s cytoskeleton and play a key role in mitosis" },
+  { question: "Vacuole", answer: "An organelle that acts as the cell’s storage sac and stores nutrients and water" },
+  { question: "Plasma (cell) membrane", answer: "A membrane that surrounds the cell and controls intake and release" },
+  { question: "Ribosome", answer: "An organelle made of RNA and protein that synthesizes protein" },
+  { question: "Nucleus", answer: "An organelle that contains all the genetic information needed for the cell to function" },
+  { question: "Nucleolus", answer: "A structure inside the nucleus that makes ribosomal RNA and forms ribosomes" }, 
+  { question: "Smooth Endoplasmic Reticulum", answer: "An organelle that synthesizes proteins; lacks ribosomes " },
+  { question: "Rough Endoplasmic Reticulum", answer: "An organelle that synthesizes, folds, modifies, and transports proteins to the Golgi Apparatus" },
+  { question: "Mitochondria", answer: "An organelle that generates ATP (energy) through cellular respiration" },
+  { question: "Golgi Apparatus", answer: "An organelle that modifies, sorts, and packages lipids and proteins from the ER into vesicles" },
+  { question: "Vesicles", answer: "Small membrane-bound sacs that transport substances in the cell" },
 ];
 
 let currentIndex = 0;
